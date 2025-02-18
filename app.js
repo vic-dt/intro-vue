4e355d36c95177d13fe71168d4660a26d09164c0
vic-dt <victoriavr1323@gmail.com>
2025-02-18 20:21:04 +0100

    añadimos frases con el input

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const { createApp, ref } = Vue;
+const { createApp, ref, computed } = Vue;
 
-const sententiae = [
+const primunSententiae = [
     { sententia: 'The night is darkest just before the dawn. And I promise you, the dawn is coming.', auctor: 'Harvey Dent, The Dark Knight' },
     { sententia: 'I believe what doesn’t kill you simply makes you, stranger.', auctor: 'The Joker, The Dark Knight' },
     { sententia: 'Your anger gives you great power. But if you let it, it will destroy you… As it almost did me', auctor: 'Henri Ducard, Batman Begins' },
@@ -14,8 +14,23 @@ const app = createApp({
 setup() {
 
     const revelareAuctor = ref(true);
+
+    const sententiae = ref(primunSententiae);
+
+    const nuntiusNovus = ref('');
+
+    const summaSententias = computed(()=>{
+        return sententiae.value.length;
+
+    });
+
     const vectisAuctor = () =>{
         revelareAuctor.value = !revelareAuctor.value;
+    }
+    const addereSententia = () =>{
+        sententiae.value.unshift({ sententia: nuntiusNovus.value, auctor:'Victoria'});
+        nuntiusNovus.value = '';
+        console.log(sententiae)
     }
 
 
@@ -23,6 +38,10 @@ setup() {
         sententiae,
         revelareAuctor,
         vectisAuctor,
+        addereSententia,
+        summaSententias,
+        nuntiusNovus,
+        
 
     }
 }
